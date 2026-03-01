0a7ac378dea85fa507b6aed38898dee8cdc50ac3
madsykle <nesbeeras3@gmail.com>
2026-03-01 07:51:54 +0000

    fix: tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteImageOptimizer({})],
+  plugins: [react(), tailwindcss(), ViteImageOptimizer({})],
   server: {
     allowedHosts: ["5173-madsykle-netflyer-cs1fk0456er.ws-us121.gitpod.io"],
   },
